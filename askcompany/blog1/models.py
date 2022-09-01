4714192a70c0a4804de50ad2594f64190e880c8b
ParkJuhyun <wngus6429@gmail.com>
2022-09-01 21:27:56 +0900

    update Study 0901
    update Study 0901

---
+++ askcompany/blog1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
