3017a5f7bc3ba158be0aa88978e7b4963d8bb432
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2022-09-06 00:51:04 +0900

    update django 0906

--- askcompany/instagram/admin.py
+++ askcompany/instagram/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+# 1번째 등록방법
+#admin.site.register(Post)
+
+# 2번째 등록방법
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(Post, PostAdmin)
+
+#3번쨰 등록방법 (이진석)
+@admin.register(Post) #wrapping, 감싸서 대상 기능을 변경가능
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'message_length', 'created_at', 'updated_at']
+    list_display_links = ['message'] #링크 클릭을 어느걸로 할건지
+
+    def message_length(self, post):
+        return f"{len(post.message)} 글자"
+        # return len(post.message)
--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -5,3 +5,11 @@ class Post(models.Model):
     message = models.TextField() #
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # return f"Custom 커스텀 Post object({self.id})"
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+    # message_length.short_description = '메세지글자수'
