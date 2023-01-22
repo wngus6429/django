cecbf0e6e1639684b14f756cee8a9e23f4165837
wngus6429 <55697824+wngus6429@users.noreply.github.com>
2023-01-23 01:13:31 +0900

    update 230122

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -5,10 +5,10 @@ from .models import Post
 
 def post_list(request):
     qs = Post.objects.all()
-    # q라는 인자가 있으면 가져온다. 없으면 none을 반환
-    print('리퀘스트', request)
+    # q라는 인자가 있으면 가져온다. 없으면 빈문자열을 반환
     q = request.GET.get('q', '')
     print('q', q)
+    # 검색어가 없다면 아래가 실행이 되지 않아서 전체가 표시됨
     if q:
         print('실행됐노')
         qs = qs.filter(message__icontains=q)
