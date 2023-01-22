8063e50e27cc0f9357e1532ca82bf94eedcb1d38
ParkJuhyun <wngus6429@gmail.com>
2023-01-22 17:44:59 +0900

    update 01/22 query,html

--- askcompany/instagram/urls.py
+++ askcompany/instagram/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from . import views
+urlpatterns = [
+    # 아무것도 없는 주소라면 views.post_list 함수 그 자체를 넘김
+    path('', views.post_list)
+]
--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from .models import Post
+
+def post_list(request):
+    qs = Post.objects.all()
+    # q라는 인자가 있으면 가져온다. 없으면 none을 반환
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(message__icontains=q)
+        # instagram/templates/instagram/post_list.html
+    return render(request, 'instagram/post_list.html', {
+        'post_list': qs
+    })
