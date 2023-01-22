8f30bdbc657164373576b79d2cf633eafba6980b
ParkJuhyun <wngus6429@gmail.com>
2023-01-22 18:57:42 +0900

    update 01/22 Query Search

--- askcompany/instagram/views.py
+++ askcompany/instagram/views.py
@@ -2,13 +2,18 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def post_list(request):
     qs = Post.objects.all()
     # q라는 인자가 있으면 가져온다. 없으면 none을 반환
+    print('리퀘스트', request)
     q = request.GET.get('q', '')
+    print('q', q)
     if q:
+        print('실행됐노')
         qs = qs.filter(message__icontains=q)
         # instagram/templates/instagram/post_list.html
     return render(request, 'instagram/post_list.html', {
-        'post_list': qs
+        'post_list': qs,
+        'q': q,
     })
