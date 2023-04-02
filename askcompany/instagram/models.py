8444d59db34f352ce94aa07056196c78be96f1ec
ParkJuhyun <wngus6429@gmail.com>
2023-04-02 17:26:36 +0900

    update 0402 django

--- askcompany/instagram/models.py
+++ askcompany/instagram/models.py
@@ -8,6 +8,7 @@ class Post(models.Model):
     message = models.TextField()
     # 이미지 라이브러리 pillow 를 설치했음, upload_to 연월일시간분초 까지 가능함.
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d', max_length=200)
+    tag_set = models.ManyToManyField('Tag', blank=True) # ManyToMany
     is_public = models.BooleanField(default=False, verbose_name='공개여부')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -29,3 +30,11 @@ class Comment(models.Model):
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    # post_set = models.ManyToManyField(Post)
+
+    def __str__(self):
+        # return f"Custom 커스텀 Post object({self.id})"
+        return self.name
